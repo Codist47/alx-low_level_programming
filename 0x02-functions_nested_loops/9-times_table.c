39e8a95701f96c54be64c82156c8d4ff92ffc591
Codist47 <kinzomoss1@gmail.com>
2023-02-21 10:45:09 -0800

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * times_table - function that prints the time table
@@ -8,5 +9,25 @@
 
 void times_table(void)
 {
+	int a;
+	int b;
+	int x;
+	int sum;
+	int chk;
 
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 0, x = 1 ; b <= 9 ; b++, x++)
+		{
+			sum = a * b;
+				chk = a * x;
+				if (chk > 9 && b != 9)
+			printf("%d, ", sum);
+			else if (b == 9)
+				printf("%d,", sum);
+			else
+				printf("%d,  ", sum);
+		}
+		putchar('\n');
+	}
 }
